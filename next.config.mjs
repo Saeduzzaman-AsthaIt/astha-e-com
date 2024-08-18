58b9131a96918cc1bbac36ad643adfb3fb6d7381
Saeduzzaman <asm.saeduzzaman@asthait.com>
2024-08-18 06:39:10 +0600

    add slider

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "img.lemde.fr",
+      "occ-0-8407-2705.1.nflxso.net",
+      "images.secretlab.co",
+    ],
+  },
 };
 
 export default nextConfig;
